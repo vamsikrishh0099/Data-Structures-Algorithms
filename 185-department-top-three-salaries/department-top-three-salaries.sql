962a140d654402dc5916222e747049a133e56247
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2024-05-05 15:41:18 -0400

    Time: 1785 ms (61.24%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -4,11 +4,11 @@
 # inner join Department dp on emp.departmentId = dp.id 
 # group by emp.departmentId ;
 
-SELECT 
-D.NAME AS Department,  E1.NAME AS EMPLOYEE, E1.Salary
-FROM EMPLOYEE E1 INNER JOIN EMPLOYEE E2 ON E1.Salary <= E2.Salary AND
- E1.DepartmentID = E2.DepartmentID JOIN Department D ON D.ID = E1.DepartmentID
- GROUP BY D.NAME,E1.NAME HAVING COUNT(distinct E2.SALARY) < 4 
+-- SELECT 
+-- D.NAME AS Department,  E1.NAME AS EMPLOYEE, E1.Salary
+-- FROM EMPLOYEE E1 INNER JOIN EMPLOYEE E2 ON E1.Salary <= E2.Salary AND
+--  E1.DepartmentID = E2.DepartmentID JOIN Department D ON D.ID = E1.DepartmentID
+--  GROUP BY D.NAME,E1.NAME HAVING COUNT(distinct E2.SALARY) < 4 
 # using dense RANK 
 # with temp as (
 # select  emp.name as employee_name, dp.name, emp.salary, dense_rank() 
@@ -23,6 +23,11 @@ FROM EMPLOYEE E1 INNER JOIN EMPLOYEE E2 ON E1.Salary <= E2.Salary AND
 # select emp1.id,emp1.name,emp1.salary as s1,emp2.salary as s2,emp1.departmentId from Employee emp1 inner join Employee emp2 on emp1.salary < emp2.salary and emp1.departmentId = emp2.departmentId
 # ) tt
 # where count(distinct s2) < 3;
+
+SELECT D.NAME AS DEPARTMENT,E1.NAME AS EMPLOYEE,E1.SALARY FROM EMPLOYEE E1 INNER JOIN EMPLOYEE E2 
+ON E1.SALARY <= E2.SALARY AND E1.DepartmentID = E2.DepartmentID
+INNER JOIN Department D ON E1.DepartmentID = D.ID 
+GROUP BY D.NAME, E1.NAME HAVING COUNT(DISTINCT (E2.SALARY)) < 4 ; 
 
 
 -- select d.name as department, e1.name as Employee, e1.salary
