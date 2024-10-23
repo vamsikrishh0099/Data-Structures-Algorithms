7d3393b5943a556d51aeda1bb597123a692ff2b5
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2024-10-22 23:00:13 -0400

    Time: 1 ms (81.68%) | Memory: 41.7 MB (63.21%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,26 +1,38 @@
 class Solution {
     public int numDecodings(String s) {
         int[] dp = new int[s.length()];
+        if (s.charAt(0) == '0'){
+            return 0;
+        }
+        Arrays.fill(dp, -1);
 
-       return helper(s,0,s.length()-1,dp);
+        return helper(s, 0, s.length()-1, dp);
+
+      //  return ans[0];
     }
 
+    int helper(String s, int start, int end, int[] dp){
 
-    int helper(String s,int start,int end,int[] dp){
-        
-         if (start == end+1) return 1;
-
-         if (dp[start]!=0) return dp[start];
-         char c= s.charAt(start);
-               if (c == '0') return 0;
-        if (start == end) return 1;
-       
-        int one = helper(s,start+1,end,dp);
-     
-    if (Integer.valueOf(s.substring(start,start+2)) < 27){
-              one += helper(s,start+2,end,dp);
+        if (start > end){
+    
+            return 1;
         }
-       
-        return dp[start] = one;
+
+        // 1 char
+
+        if (dp[start] != -1) return dp[start];
+        if (s.charAt(start) == '0') return 0;
+        int one = helper(s, start + 1, end, dp);
+
+
+        // 2 chars
+        int two = 0;
+        if (start != end && Integer.parseInt(s.substring(start,start+2)) < 27){
+            two = helper(s, start + 2, end, dp);
+        }
+
+        return dp[start] = one + two; 
+
     }
 }
+
