cd4e813c308be16cb8c08b7e5271aaa16e7dc4c3
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2024-10-28 22:59:55 -0400

    Time: 10 ms (61.54%) | Memory: 56.4 MB (42.75%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maxMoves(int[][] grid) {
         
-        int[][] visited = new int[grid.length][grid[0].length];
+        
         int[][] dp = new int[grid.length][grid[0].length];
         for (int[] x: dp) Arrays.fill(x, -1);
          int ans = 0;
         for (int i = 0; i < grid.length; i++){
-            ans = Math.max(ans, helper(grid, i,0,visited, dp));
+            ans = Math.max(ans, helper(grid, i,0, dp));
         }
         return ans; 
 
     }
 
-    int helper(int[][] grid, int i, int j, int[][] vis, int[][] dp){
+    int helper(int[][] grid, int i, int j, int[][] dp){
 
 
         int[][] moves = {{-1,1}, {0,1}, {1,1}};
 
-        vis[i][j] = 1;
+        if (dp[i][j] != -1) return dp[i][j];
+        
         int ans = 0;
         int val = grid[i][j];
 
@@ -25,10 +26,10 @@ class Solution {
             int x = i + move[0];
             int y = j + move[1];
 
-            if (x < grid.length && y < grid[0].length && x >= 0 && y >= 0 && vis[x][y] != 1 && val < grid[x][y]){
+            if (x < grid.length && y < grid[0].length && x >= 0 && y >= 0 && val < grid[x][y]){
                 
-                if (dp[x][y] != -1) return dp[x][y];
-                ans = Math.max(1 + helper(grid, x, y, vis, dp), ans);
+                
+                ans = Math.max(1 + helper(grid, x, y, dp), ans);
             }
         }
 
