b8e728aeef3ae6c0a1b518af5d00a16e3c1dda4f
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-05-18 12:15:54 -0400

    Time: 442 ms (5.01%) | Memory: 18 MB (39.90%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        candidates.sort()
+        ans = set()
+        self.helper(candidates, 0, target, [], ans)
+        return list(ans)
+
+    def helper(self, candidates, cursum, target, ds, ans) -> None:
+
+        if cursum == target:
+            sorted_ans = ds.copy()
+            sorted_ans.sort()
+            ans.add(tuple(sorted_ans))
+            return
+
+        if cursum > target:
+            return
+
+        for i in range(len(candidates)):
+            num = candidates[i]
+            ds.append(num)
+            self.helper(candidates, cursum + num, target, ds, ans)
+            ds.pop()
+
+        return
