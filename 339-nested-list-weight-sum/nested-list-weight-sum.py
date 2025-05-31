bec7358e662cbef17e7bd4a0973b4bb0b47aad59
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-05-30 21:25:57 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (97.38%) - LeetSync

---
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -0,0 +1,70 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        
+        """
+        recursive function having params input and current depth 
+        1. if integer, compute value and add to answer
+        if nestedInteger, call recursive function while increasing depth by 1
+
+        """
+        ans = [0]
+        self.helper(nestedList, ans, 1)
+
+        return ans[0]
+
+    def helper(self, input_int, ans, depth):
+
+
+        for num in input_int:
+            if num.isInteger():
+                ans[0] += num.getInteger()*depth
+            
+            else:
+                self.helper(num.getList(), ans, depth + 1)
+
+
+
+
