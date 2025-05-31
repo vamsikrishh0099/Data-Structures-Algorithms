0503bc536d84e347e0117181efbef9339e5bfe89
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-05-30 22:45:50 -0400

    Time: 0 ms (100.00%) | Memory: 18.5 MB (18.56%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -22,14 +22,14 @@ class Solution:
 
             return -1
 
-        if len(nums) == 1:
-            return 0
+        # if len(nums) == 1:
+        #     return 0
 
-        if nums[0] > nums[1]:
-            return 0
+        # if nums[0] > nums[1]:
+        #     return 0
         
-        if nums[-1] > nums[-2]:
-            return len(nums) - 1
+        # if nums[-1] > nums[-2]:
+        #     return len(nums) - 1
 
         
 
