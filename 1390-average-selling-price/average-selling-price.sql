a053005d9625657b3b59ce906ab907cfaea00879
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-01 16:47:00 -0400

    Time: 200 ms (74.08%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -7,7 +7,6 @@ US.UNITS AS UNITS
 FROM PRICES P LEFT JOIN UNITSSOLD US 
     ON P.PRODUCT_ID = US.PRODUCT_ID AND US.PURCHASE_DATE BETWEEN P.START_DATE AND P.END_DATE
 )
---select * from tt; 
 SELECT 
     tt.product_id, 
     coalesce(round((sum(tt.units*tt.price)/(sum(tt.units)*1.0)),2), 0) as average_price
