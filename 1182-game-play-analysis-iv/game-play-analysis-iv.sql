9b16a3682d8b58907e0ef96c5620038ef0678271
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-01 20:30:11 -0400

    Time: 475 ms (88.46%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -6,7 +6,7 @@ WITH TT AS (
         B.EVENT_DATE AS NEXT_DATE,
         RANK() OVER(PARTITION BY A.PLAYER_ID ORDER BY A.EVENT_DATE) AS RNK
     FROM ACTIVITY A left JOIN ACTIVITY B 
-    ON A.PLAYER_ID = B.PLAYER_ID AND A.EVENT_DATE = B.EVENT_DATE - INTERVAL '1 day'
+    ON A.PLAYER_ID = B.PLAYER_ID AND A.EVENT_DATE = B.EVENT_DATE - 1
 )
 SELECT 
     round((sum(case when next_date is null then 0 else 1 end)*1.0)/(select count(distinct player_id) from activity),2) as fraction
