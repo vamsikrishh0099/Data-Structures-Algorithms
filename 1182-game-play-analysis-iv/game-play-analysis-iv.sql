405b7ffe7eb0e132fd44b3925958bcde699e9cba
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-01 20:29:58 -0400

    Time: 483 ms (78.54%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,13 @@
+-- Write your PostgreSQL query statement below
+WITH TT AS (
+    SELECT 
+        a.player_id, 
+        A.EVENT_DATE,
+        B.EVENT_DATE AS NEXT_DATE,
+        RANK() OVER(PARTITION BY A.PLAYER_ID ORDER BY A.EVENT_DATE) AS RNK
+    FROM ACTIVITY A left JOIN ACTIVITY B 
+    ON A.PLAYER_ID = B.PLAYER_ID AND A.EVENT_DATE = B.EVENT_DATE - INTERVAL '1 day'
+)
+SELECT 
+    round((sum(case when next_date is null then 0 else 1 end)*1.0)/(select count(distinct player_id) from activity),2) as fraction
+FROM TT WHERE RNK = 1
