58fb07f32ac5a8cbe5b744aefa19de0c9b80cd3f
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-02 18:43:19 -0400

    Time: 50 ms (16.68%) | Memory: 18.3 MB (16.76%) - LeetSync

--- 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
+++ 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
@@ -8,50 +8,25 @@ class Solution:
         suffix = [1,0,2,1,0]
 
         """
-        n = len(nums)
-        prefix = []
-        suffix = [0]*n
-        c = 0
-        for i in range(0,len(nums)):
-            num = nums[i]
-
-            if num == 1:
-                c += 1
-            else:
-                c = 0
-
-            prefix.append(c)
-        
-        c = 0
-
-        if prefix[-1] == len(nums):
-            return len(nums)
-
-        for i in range(len(nums)-1, -1, -1):
-            num = nums[i]
-
-            if num == 1:
-                c += 1
-            else:
-                c = 0
-
-            suffix[i] = c
-
+        left = 0
+        right = 0
         ans = 0
+        zeros = 0
 
-        for i in range(n):
-            if nums[i] == 0:
-                left = 0
-                right = 0
+        while right < len(nums):
 
-                if i > 0 and prefix[i-1]:
-                    left = prefix[i-1]
-                if i < n - 1 and suffix[i+1]:
-                    right = suffix[i+1]
+            num = nums[right]
 
-                ans = max(ans, left + right + 1)
+            if num == 0:
+                zeros += 1
 
-        return ans 
+            while zeros == 2:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
 
-        
-            
+            ans = max(ans, right - left + 1)
+
+            right += 1
+
+        return ans
