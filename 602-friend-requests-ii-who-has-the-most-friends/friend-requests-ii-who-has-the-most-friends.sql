51e5fbed4bbae2196dbc251794f5bbad6ff12c16
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-05 01:11:07 -0400

    Time: 187 ms (39.85%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,16 +1,14 @@
-# Write your MySQL query statement below
-
-
-
-select tt.id, sum(tt.friends) as num from
-(
-select
-r1.requester_id as id, count(distinct r1.accepter_id) as friends
-from requestaccepted r1 group by r1.requester_id
-union all
-select
-r1.accepter_id as id, count(distinct r1.requester_id) as friends
-from requestaccepted r1 group by r1.accepter_id
-) tt group by tt.id order by sum(tt.friends) desc limit 1;
-
-
+-- Write your PostgreSQL query statement below
+WITH TT AS (SELECT
+REQUESTER_ID AS A,
+ACCEPTER_ID AS B
+FROM REQUESTACCEPTED 
+UNION ALL
+SELECT ACCEPTER_ID AS A,
+REQUESTER_ID AS B
+FROM  REQUESTACCEPTED
+)
+SELECT
+A AS ID, COUNT(DISTINCT B) AS NUM 
+FROM TT GROUP BY A
+ORDER BY NUM DESC LIMIT 1
