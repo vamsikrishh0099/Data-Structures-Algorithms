bc7e9614931dd98f0161787bf29996b617e9691f
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-05 00:25:35 -0400

    Time: 278 ms (32.83%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,23 +1,13 @@
-# Write your MySQL query statement below
-
-
-
-select round(sum(t.TIV_2016),2) as tiv_2016 from insurance t
-where (lat,lon) not in (
-select lat, lon from insurance where pid != t.pid 
-) and 
-t.tiv_2015 in (select tiv_2015 from insurance where pid != t.pid )
-
-
--- SELECT
---     SUM(TIV_2016) AS TIV_2016
--- FROM insurance
--- WHERE (LAT, LON) IN (SELECT
---                         LAT,
---                         LON
---                      FROM insurance
---                      GROUP BY LAT, LON
---                      HAVING COUNT(*) = 1) AND
---        TIV_2015 IN (SELECT TIV_2015 FROM insurance GROUP BY TIV_2015 HAVING COUNT(*) >= 2);
-
-
+-- Write your PostgreSQL query statement below
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+SELECT
+ROUND(SUM(I.TIV_2016)::DECIMAL,2) AS TIV_2016
+FROM INSURANCE I 
+WHERE I.TIV_2015 IN (SELECT TIV_2015 FROM INSURANCE I2 WHERE I.PID <> I2.PID ) AND 
+(I.LAT,I.LON) NOT IN (SELECT I3.LAT, I3.LON FROM INSURANCE I3 WHERE I.PID <> I3.PID)
