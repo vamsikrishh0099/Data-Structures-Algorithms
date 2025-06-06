ceff7a785c93205ba7870f1f144806f485462b23
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-06 18:24:47 -0400

    Time: 11 ms (69.64%) | Memory: 23.7 MB (5.37%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.py
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.py
@@ -5,7 +5,6 @@ class Solution:
         1. make all other words as small as possible. 
         2. size - (num_friends - 1) Ex: 4 - 1 = 3, 4 - 3 = 1
         3. once we have size of answer, append all possible answers to a list. sort desc and take first. 
-
         """
 
         if numFriends == 1:
@@ -22,9 +21,13 @@ class Solution:
         for i in range(n):
             if word[i] == max_char:
                 all_ans.append(word[i:i + ans_size])
+                
         
         if len(all_ans) == 1:
             return all_ans[0]
         
         all_ans.sort(reverse = True)
         return all_ans[0]
+
+
+
