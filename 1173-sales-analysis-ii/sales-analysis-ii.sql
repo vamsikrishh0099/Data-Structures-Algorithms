f32802eadd468a885ec9eb1dfd946ec10786c1fb
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-18 14:13:02 -0400

    Time: 941 ms (44.12%) | Memory: 0B (100.00%) - LeetSync

--- 1173-sales-analysis-ii/sales-analysis-ii.sql
+++ 1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -1,13 +1,16 @@
-# Write your MySQL query statement below
-WITH CTE1 AS (
-select BUYER_ID,P.PRODUCT_ID,P.PRODUCT_NAME FROM SALES S JOIN PRODUCT P ON S.PRODUCT_ID = P.PRODUCT_ID
-),
- cte2 as (
-SELECT c1.BUYER_ID FROM CTE1 c1 WHERE c1.PRODUCT_NAME = 'S8'
-),
-cte3 as (
-SELECT c1.BUYER_ID FROM CTE1 c1 WHERE c1.PRODUCT_NAME = 'iPhone'
+-- Write your PostgreSQL query statement below
+WITH TT AS (
+    SELECT
+    P.PRODUCT_ID, 
+    P.PRODUCT_NAME,
+    S.BUYER_ID
+    FROM SALES S JOIN PRODUCT P 
+    ON S.PRODUCT_ID = P.PRODUCT_ID 
+    WHERE P.PRODUCT_NAME IN ('S8', 'iPhone')
 )
-select distinct cte2.buyer_id from cte2 left join cte3 
-on cte2.BUYER_ID = cte3.BUYER_ID
-where cte3.BUYER_ID is null;
+SELECT
+    DISTINCT BUYER_ID
+FROM TT WHERE PRODUCT_NAME = 'S8' 
+ AND BUYER_ID NOT IN (
+    SELECT BUYER_ID FROM TT WHERE PRODUCT_NAME = 'iPhone'
+ )
