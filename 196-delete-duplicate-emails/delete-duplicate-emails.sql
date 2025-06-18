90cbe03ec16cdba56f1842c4ad38a1e90db3abaa
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-18 17:19:05 -0400

    Time: 251 ms (36.50%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,8 +1,8 @@
 -- Write your PostgreSQL query statement below
 delete from person 
-where id in (
-    select id from (
-    select id, rank() over(partition by email order by id) as rnk 
-    from person
-    ) where rnk > 1
+where id not in (
+    select
+     min(id)
+    from person group by email 
+    
 )
