9c76c43f212d9450491ce15e47b83009efae7bd1
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-18 00:53:07 -0400

    Time: 23 ms (52.02%) | Memory: 19.9 MB (65.61%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -1,27 +1,29 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
 
-        g.sort(reverse=True)
-        s.sort(reverse=True)
+        g.sort(reverse = True)
+        s.sort(reverse = True)
+
+        greed_i = 0
+        cookie_i = 0
         ans = 0
-        cookie_index = 0
-        greed_index = 0
 
-        while cookie_index < len(s):
-            size = s[cookie_index]
+        while cookie_i < len(s):
+            size = s[cookie_i]
 
-            while greed_index < len(g) and g[greed_index] > size:
-                greed_index += 1
+            while greed_i < len(g) and g[greed_i] > size:
+                greed_i += 1
 
-            if greed_index == len(g):
-                return ans 
+            if greed_i == len(g):
+                break
 
             ans += 1
-            cookie_index += 1
-            greed_index += 1
-            
-        return ans 
+            cookie_i += 1
+            greed_i += 1
 
-        #g = [2,1] s = [3,2,1]
+        return ans
+
+            
+
 
 
