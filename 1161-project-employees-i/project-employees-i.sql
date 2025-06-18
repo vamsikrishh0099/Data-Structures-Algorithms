343f9f77ab6879b3d152d2f3a5c87a0bba910976
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-18 14:28:40 -0400

    Time: 492 ms (26.99%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,5 +1,7 @@
-# Write your MySQL query statement below
-
-select p.project_id, round(sum(IFNULL(e.experience_years,0))/count(distinct(p.employee_id)),2) as average_years from project p 
-left join employee e on p.employee_id = e.employee_id where e.experience_years is not null
+-- Write your PostgreSQL query statement below
+select
+p.project_id,
+round(avg(coalesce(e.experience_years, 0)),2) as average_years
+from project p left join employee e 
+on p.employee_id = e.employee_id 
 group by p.project_id
