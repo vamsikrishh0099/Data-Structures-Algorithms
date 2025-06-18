a405b3f9a0584a4b6aee94655b9ba84999b295b0
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-18 14:32:30 -0400

    Time: 537 ms (15.17%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,7 +1,7 @@
 -- Write your PostgreSQL query statement below
 select
 p.project_id,
-round(avg(coalesce(e.experience_years, 0)),2) as average_years
+round(sum(coalesce(e.experience_years,0))::numeric/(count(e.employee_id)),2) as average_years
 from project p left join employee e 
 on p.employee_id = e.employee_id 
 group by p.project_id
