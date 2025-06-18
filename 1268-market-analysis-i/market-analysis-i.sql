e0a0a8b746ef2fcc893c0d6cb3a6f7850e9f4c51
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-18 19:22:30 -0400

    Time: 525 ms (66.43%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,7 +1,8 @@
 -- Write your PostgreSQL query statement below
 SELECT
     U.USER_ID AS BUYER_ID,
-    U.JOIN_DATE,
-    COALESCE(COUNT(case when extract(year from o.order_date) = 2019 then 1 else null end), 0) AS orders_in_2019
+    u.JOIN_DATE,
+    COALESCE(COUNT(o.order_id), 0) AS orders_in_2019
 FROM USERS U LEFT JOIN ORDERS O ON U.USER_ID = O.BUYER_ID 
-GROUP BY U.USER_ID, U.JOIN_DATE
+and EXTRACT(YEAR FROM O.ORDER_DATE) = '2019' 
+GROUP BY U.USER_ID,u.join_date
