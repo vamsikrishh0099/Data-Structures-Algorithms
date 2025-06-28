8113d8ac88f0ab094d73a477c4ffca02fa464db1
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-28 17:37:21 -0400

    Time: 1 ms (91.90%) | Memory: 18.3 MB (12.29%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,41 +1,15 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        sorted_list = [(val, ind) for ind, val in enumerate(nums)]
 
-        """
-        This is not the best solution at all. 
-        Not ideal for 0/1 knapsack as it is a overkill. 
-        sorting and picking top k elements is better approach. 
-        """
-        n = len(nums)
-        # Memoization table: dp[index][count] = (max_sum, subsequence)
-        dp = {}
-        
-        def knapsack(index: int, count: int) -> tuple[int, List[int]]:
-            # Base cases
-            if count == k:
-                return 0, []
-            if index >= n or n - index < k - count:  # Not enough elements left
-                return float('-inf'), []
-            if (index, count) in dp:
-                return dp[(index, count)]
-            
-            # Include current element
-            inc_sum, inc_seq = knapsack(index + 1, count + 1)
-            inc_sum += nums[index]
-            inc_seq = [nums[index]] + inc_seq
-            
-            # Exclude current element
-            exc_sum, exc_seq = knapsack(index + 1, count)
-            
-            # Choose the option with the maximum sum
-            if inc_sum > exc_sum:
-                dp[(index, count)] = (inc_sum, inc_seq)
-            else:
-                dp[(index, count)] = (exc_sum, exc_seq)
-                
-            return dp[(index, count)]
-        
-        # Start from index 0 with count 0
-        return knapsack(0, 0)[1]
+        sorted_list.sort(reverse = True)
 
-        
+        picked_nums = [(num, ind) for (num, ind) in sorted_list[:k]]
+
+        picked_nums.sort(key = lambda x: x[1])
+
+        for i in range(len(picked_nums)):
+
+            picked_nums[i] = picked_nums[i][0]
+
+        return picked_nums
