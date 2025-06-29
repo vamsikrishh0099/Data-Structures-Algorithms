6feed1796b483ae9e22508ca18f970b938ea7a4f
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-06-29 00:11:26 -0400

    Time: 155 ms (24.17%) | Memory: 19 MB (90.00%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,5 +1,20 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        """
+        heap + queue
+
+        we dont need characters. just freq. 
+        we use a maxheap to get the most occuring task.
+        once we process it, we add to queue (remaining, next time to run)
+        then in next iteration, we check front of queue.
+            if front of queue is ready for execution, we add it to maxheap
+            if not, we do nothing to queue. 
+        we poll from maxheap and process it and decrease count and add it back to queue.
+
+        in this process, if maxheap is empty, it means we just wait. 
+        
+
+        """
         pq = []
         mp = {}
 
@@ -18,11 +33,11 @@ class Solution:
 
             if q and q[0][1] < timer:
                 heapq.heappush(pq, -q.popleft()[0])
-            
+
             if not pq:
                 continue
+
             cur_task = -heapq.heappop(pq)
-            
 
             cur_task = cur_task - 1
 
