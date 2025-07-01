c155eff4927fe5b9ef17ccdfc0d737bb4015ffd9
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-07-01 01:18:23 -0400

    Time: 363 ms (20.20%) | Memory: 68.5 MB (6.24%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
@@ -1,21 +1,28 @@
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-
+        """ 
+        first sort intervals by start date.
+        sort queries. keep track of index positions as we need answers array in same order.
+        Iterate over queries:
+        for each query, insert all possible intervals into minheap. (interval_size, end)
+        the idea is, this way, after all inserts, the heap top will be shortest interval size. If tie, 
+        we choose the one ending sooner. hence, (interval_size, end) is used in heap.
+        before we take top of minheap as answer, we need to pop invalid elements in minheap.
+        if top of minheap end is less than query, it is invalid, pop it.
+        if element in heap, that is answer. else -1
+        """
         intervals.sort(key=lambda x: x[0])
 
-        queries = [(q,i) for i,q in enumerate(queries)]
-        queries.sort(key = lambda x: x[0])
-
+        queries = [(q, i) for i, q in enumerate(queries)]
+        queries.sort(key=lambda x: x[0])
 
         pq = []
         interval_index = 0
         ans = []
 
-        for q,i in queries:
+        for q, i in queries:
 
-            while (
-                interval_index < len(intervals) and intervals[interval_index][0] <= q
-            ):
+            while interval_index < len(intervals) and intervals[interval_index][0] <= q:
                 heapq.heappush(
                     pq,
                     (
@@ -32,10 +39,8 @@ class Solution:
                 ans.append((pq[0][0], i))
             else:
                 ans.append((-1, i))
-        
-        ans.sort(key = lambda x: x[1])
-        ans = [val for val,index in ans]
+
+        ans.sort(key=lambda x: x[1])
+        ans = [val for val, index in ans]
 
         return ans
-
-            
