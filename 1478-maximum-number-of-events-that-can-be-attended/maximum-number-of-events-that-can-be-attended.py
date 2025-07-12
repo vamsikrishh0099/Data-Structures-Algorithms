2c4fdad4cf66a71b20da79f1fabf989cbbf62e70
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-07-12 17:02:44 -0400

    Time: 143 ms (66.43%) | Memory: 53.3 MB (57.13%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,6 +1,14 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        
+        """
+        Use minheap to keep track of end dates of events after sorting by (start, end)
+        we need to prioritize attending events that end early. Therefore, push end times into minheap.
+
+        at each event iteration, push all events end day that start on current_day
+        this way, the top of minheap will be event that is ending the earliest.
+        we attend that. increase count by 1.  increase current date by 1. 
+        we clean up minheap by popping all events whose end date is less than current date
+        """
         events.sort(key = lambda x: (x[0], x[1]))
 
         pq = []
