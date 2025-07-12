c4a43f0ae79138e43dc80dcc3644ffb433e40c75
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-07-12 16:57:34 -0400

    Time: 348 ms (38.57%) | Memory: 64.1 MB (7.69%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
@@ -1,5 +1,8 @@
 class FindSumPairs:
-
+    """
+    if you need to return indices, the map should be: num -> list of indices
+    if just count, the map can be: num -> counts (freq counter)
+    """
     def __init__(self, nums1: List[int], nums2: List[int]):
         
         self.nums2_map = {}
@@ -9,19 +12,18 @@ class FindSumPairs:
         self.nums2 = nums2
 
         for i,num in enumerate(nums2):
-            self.nums2_map[num] = self.nums2_map.get(num, set())
-            self.nums2_map[num].add(i)
+            self.nums2_map[num] = self.nums2_map.get(num, 0) + 1
             self.imap[i] = num
 
     def add(self, index: int, val: int) -> None:
         
         curnum = self.imap[index]
-        self.nums2_map[curnum].remove(index)
+        self.nums2_map[curnum] -= 1
         self.imap[index] = curnum + val
         
         if self.imap[index] not in self.nums2_map:
-            self.nums2_map[self.imap[index]] = set()
-        self.nums2_map[self.imap[index]].add(index)
+            self.nums2_map[self.imap[index]] = 0
+        self.nums2_map[self.imap[index]] += 1
 
 
 
@@ -29,7 +31,7 @@ class FindSumPairs:
         ans = 0
         for i, val in enumerate(self.nums1):
             if tot - val in self.nums2_map:
-                ans += len(self.nums2_map[tot - val])
+                ans += self.nums2_map[tot - val]
 
         return ans
 
