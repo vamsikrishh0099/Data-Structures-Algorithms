2da08e38c09ee9e9131683adaaf7c28e2c12a104
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-08-31 14:19:21 -0400

    Time: 4 ms (53.73%) | Memory: 19.9 MB (76.02%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,40 +1,22 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        """
-        sort the input by start time.
-        pq.
-        insert end times into pq as you iterate. 
-        for every num, check if top is less than current meeting start.
-        if yes, pop and insert new meeting end
-        if no, just insert new meeting end 
-        ans = max(ans, len(pq))
-        """
+        
+        intervals.sort(key = lambda x: x[0])
 
-        # pq = []
-        # ans = 0
-        # intervals.sort(key = lambda x: x[0]) 
-
-        # for interval in intervals:
-        #     if pq and pq[0] <= interval[0]:
-        #         heapq.heappop(pq)
-            
-        #     heapq.heappush(pq, interval[1])
-
-        #     ans = max(ans, len(pq))
-        # return max(ans, len(pq))
-
-        total = []
-        for interval in intervals:
-            total.append((interval[0], 1))
-            total.append((interval[1], -1))
-
-        total.sort()
+        pq = []
 
         ans = 0
-        cur = 0
-        for time in total:
-            cur += time[1]
-            ans = max(cur, ans)
 
-        return ans
+        for interval in intervals:
+            if not pq:
+                heapq.heappush(pq, interval[1])
+                continue
             
+            ans = max(ans, len(pq))
+
+            if pq[0] <= interval[0]:
+                 heapq.heappop(pq)
+
+            heapq.heappush(pq, interval[1])
+
+        return max(ans, len(pq))
