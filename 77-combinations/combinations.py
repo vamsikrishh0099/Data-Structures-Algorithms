3cae10d43df092877d59da76ed24cf3b18389b33
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2025-09-05 00:30:04 -0400

    Time: 135 ms (64.67%) | Memory: 59.8 MB (15.32%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -7,18 +7,38 @@ class Solution:
         iterate over list from ind to end, and pick or not pick
         """
 
-        def helper(ind, cur, ans):
+        # def helper(ind, cur, ans):
+
+        #     if len(cur) == k:
+        #         ans.append(cur.copy())
+        #         return 
+
+        #     for i in range(ind, n + 1):
+        #         cur.append(i)
+        #         helper(i + 1, cur, ans)
+        #         cur.pop()
+
+        # ans = []
+        # helper(1, [], ans)
+
+        # return ans
+
+        def helper(ind, cur, k, ans):
 
             if len(cur) == k:
                 ans.append(cur.copy())
-                return 
+                return
 
-            for i in range(ind, n + 1):
-                cur.append(i)
-                helper(i + 1, cur, ans)
-                cur.pop()
+            if ind > n:
+                return
+            
+            cur.append(ind)
+            helper(ind + 1, cur, k, ans)
+            cur.pop()
+            helper(ind + 1, cur, k, ans)
+
 
         ans = []
-        helper(1, [], ans)
+        helper(1, [], k, ans)
 
         return ans
